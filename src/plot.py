43969d3b2d4e0eaf241c5e0930a81b8923b68932
srijarko <sr8962@srmist.edu.in>
2021-03-25 17:53:37 +0530

    added plot.py

---
+++ src/plot.py
@@ -0,0 +1,17 @@
+def plot_mixture_sources_predictions(X, original_sources, S):
+  fig = plt.figure()
+  plt.subplot(3, 1, 1)
+  for x in X:
+    plt.plot(x)
+  plt.title("Mixed signals")
+  plt.subplot(3, 1, 2)
+  for s in original_sources:
+    plt.plot(s)
+  plt.title("Real source signals")
+  plt.subplot(3,1,3)
+  for s in S:
+    plt.plot(s)
+  plt.title("Predicted source signals")
+
+  fig.tight_layout()
+  plt.show()
